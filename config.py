3434c76e62a349cc42dacff2c9b2ef68a5351b2e
Apnijanta <159753576+Apnijanta@users.noreply.github.com>
2024-07-03 16:20:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-SESSION = getenv("PYROGRAM_V2_SESSION", "")
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "29939868"))
+API_HASH = getenv("API_HASH", "1ee10fce7cb72aaf315b97f4b33decc0")
+BOT_TOKEN = getenv("BOT_TOKEN", "6746489857:AAFOGAmubH16HgoYiTQmC-yRjr6gMg9okNI")
+OWNER_ID = int(getenv("OWNER_ID", "5359880114"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://pranchal1419:<password>@cluster0.gicdshq.mongodb.net/")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002198337850"))
+SESSION = getenv("PYROGRAM_V2_SESSION", "BQHI2JwAZYG7luLxl_5icYBeQ90WxwDENmedVyAa9qMEEWMUqzaHGYw0s2IXPDcmxoB3FCmUnzGK713byonZDmH82fsupGG9DHTip174MzkHzxqSKfGGwP01gVweWZmHXNLGz7X6TaT0eFfW0W1IWsyJMPDf5Gsi1aRtStbaoal85yRJwYuEuvu5sq4VS_Kf0D75bwXrvoI9_V5ERSSu7jUuOK3bUTFHXafneoP7EX5z_te4INbBrVhm3x01ej3-pbGCrlzfnIubIlhHiTx8uRh9WmV34Sut44LlaXILu9hRNQlIWaBAc0zC0CsgWOimu4dHOZgUG_gXGPSAEs7vJ9szCo64ZwAAAAE_eUeyAA")
+FORCESUB = getenv("FORCESUB", "aravsave")
