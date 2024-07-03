02f67d0aa4fb64a873bdca2d3765e1d3e41dd03e
Apnijanta <159753576+Apnijanta@users.noreply.github.com>
2024-07-03 17:03:22 +0530

    Update pyroplug.py

--- ggn/assets/pyroplug.py
+++ ggn/assets/pyroplug.py
@@ -208,8 +208,8 @@ async def _unauth(event):
 # Note you can modify the commands if you need authorization for use and want for commercial purposes...
 # Edit '#' before rename setchat setcaption etc if you want to restrict them for not using this 
 
-API_ID = "19748984" 
-API_HASH = "2141e30f96dfbd8c46fbb5ff4b197004"
+API_ID = "29939868" 
+API_HASH = "1ee10fce7cb72aaf315b97f4b33decc0"
 
 async def check(userbot, client, link, event):
     logging.info(link)
