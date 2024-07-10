81efe0a62a2d08889e289b4c388e368430a146bd
Apnijanta <159753576+Apnijanta@users.noreply.github.com>
2024-07-10 19:33:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "29939868"))
-API_HASH = getenv("API_HASH", "1ee10fce7cb72aaf315b97f4b33decc0")
-BOT_TOKEN = getenv("BOT_TOKEN", "6902646629:AAGV9If7gxVSpbOYEudIK6hu9wKsp7JlnmM")
-OWNER_ID = int(getenv("OWNER_ID", "5359880114"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://pranchal1419:<password>@cluster0.gicdshq.mongodb.net/")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1002198337850"))
-SESSION = getenv("PYROGRAM_V2_SESSION", "BQHI2JwApN2nOq22H1crU1o78U-6Y3vudTCeJ2p-SpO6slki9Ly_wnTzpHBneOmamIsxg-i8JdS1uF22h8c1eH_DFC-Is2ygSZkuqbHwE0XGSdRaOmVrfPs9lUicvej3ONsd2T5r51dVox_vuhzk8CA7YKVFeneOGEJgRMJPkRvb9syzZb-ilwOxsfMGalRIj8IoPkyYxGFcWKxnnLndhP5Fv3bOQUbxjm-9wqoOEvyp0xti-TjPQL5QprHIDCQfTvdWWtstE9CKwfFP4h28eYHqLf6Qe-t4AC63CbIXs2epDP31D4wY2T02qRI5i9xT-TZgp7o-EZlVWzsLZJdgNXkAdEp1sQAAAAE_eUeyAA")
-FORCESUB = getenv("FORCESUB", "aravsave")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+MONGODB_CONNECTION_STRING = getenv("")
+LOG_GROUP = int(getenv("LOG_GROUP", ""))
+SESSION = getenv("PYROGRAM_V2_SESSION", "")
+FORCESUB = getenv("FORCESUB", "")
